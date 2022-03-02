6e9866a7d8436fb66321bf8221cea96ee2982102
Kirk <kfournia@gmail.com>
2022-03-02 16:54:48 +0200

    Added Constants

---
+++ src/utils/auctionHouse/helpers/constants.ts
@@ -0,0 +1,77 @@
+import { PublicKey } from '@solana/web3.js';
+export const CANDY_MACHINE = 'candy_machine';
+export const AUCTION_HOUSE = 'auction_house';
+export const TOKEN_ENTANGLER = 'token_entangler';
+export const ESCROW = 'escrow';
+export const A = 'A';
+export const B = 'B';
+export const FEE_PAYER = 'fee_payer';
+export const TREASURY = 'treasury';
+export const LAMPORT = 1000000000;
+export const MAX_NAME_LENGTH = 32;
+export const MAX_URI_LENGTH = 200;
+export const MAX_SYMBOL_LENGTH = 10;
+export const MAX_CREATOR_LEN = 32 + 1 + 1;
+
+export const ARWEAVE_PAYMENT_WALLET = new PublicKey(
+  '6FKvsq4ydWFci6nGq9ckbjYMtnmaqAoatz5c9XWjiDuS',
+);
+export const METAPLEX_PROGRAM_ID = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
+export const CANDY_MACHINE_PROGRAM_ID = new PublicKey(
+  'cndyAnrLdpjq1Ssp1z8xxDsB8dxe7u4HL5Nxi2K5WXZ',
+);
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+  'metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s',
+);
+export const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+);
+export const TOKEN_PROGRAM_ID = new PublicKey(
+  'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+);
+export const FAIR_LAUNCH_PROGRAM_ID = new PublicKey(
+  'faircnAB9k59Y4TXmLabBULeuTLgV7TkGMGNkjnA15j',
+);
+export const AUCTION_HOUSE_PROGRAM_ID = new PublicKey(
+  'hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk',
+);
+export const TOKEN_ENTANGLEMENT_PROGRAM_ID = new PublicKey(
+  'qntmGodpGkrM42mN68VCZHXnKqDCT8rdY23wFcXCLPd',
+);
+export const WRAPPED_SOL_MINT = new PublicKey(
+  'So11111111111111111111111111111111111111112',
+);
+export const GUMDROP_DISTRIBUTOR_ID = new PublicKey(
+  'gdrpGjVffourzkdDRrQmySw4aTHr8a3xmQzzxSwFD1a'
+);
+export const GUMDROP_TEMPORAL_SIGNER = new PublicKey(
+  'MSv9H2sMceAzccBganUXwGq3GXgqYAstmZAbFDZYbAV'
+);
+
+export const CONFIG_ARRAY_START =
+  32 + // authority
+  4 +
+  6 + // uuid + u32 len
+  4 +
+  10 + // u32 len + symbol
+  2 + // seller fee basis points
+  1 +
+  4 +
+  5 * 34 + // optional + u32 len + actual vec
+  8 + //max supply
+  1 + //is mutable
+  1 + // retain authority
+  4; // max number of lines;
+export const CONFIG_LINE_SIZE = 4 + 32 + 4 + 200;
+
+export const CACHE_PATH = './.cache';
+
+export const DEFAULT_TIMEOUT = 15000;
+
+export const EXTENSION_PNG = '.png';
+export const EXTENSION_JSON = '.json';
+
+export const ENV_AH = "mainnet-beta";
+export const AUCTION_HOUSE_ADDRESS = "4w2BVBfV86NBm3ytL1AuHxToBV7Kx5YahdMRgyyYFoRj";
---
+++ src/utils/grapeTools/constants.ts
@@ -0,0 +1,109 @@
+export const FREE_RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com';
+export const GRAPE_RPC_ENDPOINT = 'https://api.mainnet-beta.solana.com';
+
+export const GRAPE_RPC_REFRESH = 25000;
+export const GRAPE_TREASURY = 'GrapevviL94JZRiZwn2LjpWtmDacXU8QhAJvzpUMMFdL';
+
+export const TOKEN_VERIFICATION_ADDRESS = '8upjSpvjcdpuzhfR1zriwg5NXkwDruejqNE9WNbPRtyA';
+export const TOKEN_VERIFICATION_NAME = 'Grape';
+export const TOKEN_VERIFICATION_AMOUNT = 1000;
+export const TOKEN_REALM_ID = 'By2sVGZXwfQq6rAiAM3rNPJ9iQfb5e2QhnF4YjJ4Bip';
+export const TOKEN_REALM_PROGRAM_ID = 'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw';
+
+export const FEATURED_DAO_ARRAY = [{
+    address:'66pJhhESDjdeBBDdkKmxYYd7q6GUggYPWjxpMKNX39KV',
+    daourl:'https://realms.today/dao/Ukraine',
+    img:'/solana4ukraine.png',
+    title:'Solana for Ukraine',
+    text:'NFT Artists come together in the DAO made exlusively to help out Ukraine, all proceeds of these NFT\'s will be donated to help the Ukrainian people'
+}]
+
+//export const GRAPE_PREVIEW = '/preview/';
+//export const GRAPE_PROFILE = '/profile/';
+
+export const GRAPE_PREVIEW = '/preview?pkey=';
+export const GRAPE_PROFILE = '/profile?pkey=';
+
+export const OTHER_MARKETPLACES = new Array(
+    {
+        name: 'Magic Eden',
+        logo: 'https://magiceden.io/static/media/logo.ca418d75.svg',
+        //logo: 'https://magiceden.io/img/logo.png',
+        address: 'GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp',
+        previewUrl: 'https://www.magiceden.io/item-details/',
+        url: 'https://www.magiceden.io'
+    },{
+        name: 'SolanArt',
+        logo: 'https://solanart.io/static/media/logo.0054f7e7.png',
+        address: '3D49QorJyNaL4rcpiynbuS3pRH4Y7EXEM6v6ZGaqfFGK',
+        previewUrl: 'https://solanart.io/search/?token=',
+        url: 'https://solanart.io'
+    },{
+        name: 'Digital Eyes',
+        logo: 'https://ik.imagekit.io/srjnqnjbpn9/logo/digitaleyes.svg?ik-sdk-version=react-1.0.11',
+        //logo: 'https://ik.imagekit.io/favicon-32x32.png',
+        address: 'F4ghBzHFNgJxV4wEQDchU5i7n4XWWMBSaq7CuswGiVsr',
+        previewUrl: '',
+        url: 'https://www.magiceden.io'
+    },{
+        name: 'Digital Eyes',
+        logo: 'https://ik.imagekit.io/srjnqnjbpn9/logo/digitaleyes.svg?ik-sdk-version=react-1.0.11',
+        //logo: 'https://ik.imagekit.io/srjnqnjbpn9/logo/favicon-32x32.png',
+        address: 'BweTPKW9QsWZTJnfbLz88ekKaWw7HrBJsStrduHryPgk',
+        previewUrl: '',
+        url: 'https://www.magiceden.io'
+    },{
+        name: 'Alph Art',
+        //logo: 'https://alpha.art/icon-root.svg',
+        logo: 'https://alpha.art/logo_192.png',
+        address: '4pUQS4Jo2dsfWzt3VgHXy3H6RYnEDd11oWPiaM2rdAPw',
+        previewUrl: 'https://alpha.art/t/',
+        url: 'https://www.alpha.art'
+    },{
+        name: 'FTX',
+        logo: 'https://ftx.us/static/media/ftxus_logo_white.b384ac52.svg',
+        address: '73tF8uN3BwVzUzwETv59WNAafuEBct2zTgYbYXLggQiU',
+        previewUrl: '',
+        url: 'https://ftx.us/nfts'
+    },{
+        name: 'FTX',
+        logo: 'https://ftx.us/static/media/ftxus_logo_white.b384ac52.svg',
+        address: 'HHvnfyY7vNWhjeHqCVyMPr4UjDp3ptqu3QbuFoCkrm8r',
+        previewUrl: '',
+        url: 'https://ftx.us/nfts'
+    },{
+        name: 'FTX',
+        logo: 'https://ftx.us/static/media/ftxus_logo_white.b384ac52.svg',
+        address: 'HznNaC2cz1iXMtHMG3HAgMH2xkrt2iTXfUKz6wwAAVPB',
+        previewUrl: '',
+        url: 'https://ftx.us/nfts'
+    },{
+        name: 'SMB Market',
+        //logo: 'https://market.solanamonkey.business/logo/smb-market.svg',
+        logo: 'https://market.solanamonkey.business/logo/smb.png',
+        address: 'G6xptnrkj4bxg9H9ZyPzmAnNsGghSxZ7oBCL1KNKJUza',
+        previewUrl: 'https://market.solanamonkey.business/item/',
+        url: 'https://market.solanamonkey.business/'
+    },{
+        name: 'SMB Market',
+        //logo: 'https://market.solanamonkey.business/logo/smb-market.svg',
+        logo: 'https://market.solanamonkey.business/logo/smb.png',
+        address: '7Ppgch9d4XRAygVNJP4bDkc7V6htYXGfghX4zzG9r4cH',
+        previewUrl: 'https://market.solanamonkey.business/item/',
+        url: 'https://market.solanamonkey.business/'
+    },{
+        name: 'SMB Market',
+        //logo: 'https://market.solanamonkey.business/logo/smb-market.svg',
+        logo: 'https://market.solanamonkey.business/logo/smb.png',
+        address: 'EQEpTS8SiVPUxyqRgT2Jdx9dHEwbpcbSZ2sotiAUZ7i7',
+        previewUrl: 'https://market.solanamonkey.business/item/',
+        url: 'https://market.solanamonkey.business/'
+    },{
+        name: 'SMB Market',
+        //logo: 'https://market.solanamonkey.business/logo/smb-market.svg',
+        logo: 'https://market.solanamonkey.business/logo/smb.png',
+        address: 'CPo76cw52gKMTyvCS9GpnnZfvmkQQxovLwQFxonZrCRU',
+        previewUrl: 'https://market.solanamonkey.business/item/',
+        url: 'https://market.solanamonkey.business/'
+    }
+);
