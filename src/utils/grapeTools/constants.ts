06b06797bcd04b326ccded683767559eafbbb932
Kirk <kfournia@gmail.com>
2022-03-04 01:59:35 +0200

    updated router
    Browser router rewriting prep

--- src/utils/grapeTools/constants.ts
+++ src/utils/grapeTools/constants.ts
@@ -2,11 +2,11 @@
 export const FREE_RPC_ENDPOINT = process.env.REACT_APP_API_FREE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 export const GRAPE_RPC_ENDPOINT = process.env.REACT_APP_API_GRAPE_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 
-export const GRAPE_PREVIEW = '/preview/';
-export const GRAPE_PROFILE = '/profile/';
+//export const GRAPE_PREVIEW = '/preview/';
+//export const GRAPE_PROFILE = '/profile/';
 
-//export const GRAPE_PREVIEW = '/preview?pkey=';
-//export const GRAPE_PROFILE = '/profile?pkey=';
+export const GRAPE_PREVIEW = '/preview?pkey=';
+export const GRAPE_PROFILE = '/profile?pkey=';
 
 export const GRAPE_RPC_REFRESH = 25000;
 export const GRAPE_TREASURY = 'GrapevviL94JZRiZwn2LjpWtmDacXU8QhAJvzpUMMFdL';
