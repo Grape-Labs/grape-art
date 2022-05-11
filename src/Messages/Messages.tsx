e5fad8de1ffecfc865462e2fc3613a41e03684e0
Kirk <kfournia@gmail.com>
2022-05-11 10:37:01 +0300

    prep-work for dialect integration

---
+++ src/Messages/Messages.tsx
@@ -0,0 +1,73 @@
+import '@dialectlabs/react-ui/index.css';
+import {
+    ChatButton,
+    NotificationsButton,
+    IncomingThemeVariables,
+    defaultVariables,
+  } from '@dialectlabs/react-ui';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { PublicKey } from '@solana/web3.js';
+
+import {
+    Box,
+  } from '@mui/material';
+  import {
+    AUCTION_HOUSE_PROGRAM_ID,
+    AUCTION_HOUSE_ADDRESS,
+  } from '../utils/auctionHouse/helpers/constants';
+
+  export const themeVariables: IncomingThemeVariables = {
+    dark: {
+      bellButton:
+        'w-10 h-10 shadow-xl shadow-neutral-800 border border-neutral-600 hover:shadow-neutral-700 bg-white text-black',
+      modal: `${defaultVariables.dark.modal} sm:rounded-3xl shadow-xl shadow-neutral-900 sm:border border-[#ABABAB]/40`, // 0.4 opacity based on trial-and-error
+    },
+    animations: {
+      popup: {
+        enter: 'transition-all duration-300 origin-top-right',
+        enterFrom: 'opacity-0 scale-75',
+        enterTo: 'opacity-100 scale-100',
+        leave: 'transition-all duration-100 origin-top-right',
+        leaveFrom: 'opacity-100 scale-100',
+        leaveTo: 'opacity-0 scale-75',
+      },
+    },
+  };
+
+export function MessagesView(){
+    // ...
+    const wallet = useWallet();
+    const theme: 'dark' | 'light' = 'dark';
+    //const YOUR_PROJECT_PUBLIC_KEY = new PublicKey(AUCTION_HOUSE_ADDRESS);
+    const DIALECT_PUBLIC_KEY = new PublicKey(
+        'D2pyBevYb6dit1oCx6e8vCxFK9mBeYCRe8TTntk2Tm98'
+    );
+    
+    return (
+
+        <Box
+                    sx={{ 
+                        p: 1, 
+                        mt: 6, 
+                        mb: 3, 
+                        width: '100%',
+                        background: '#13151C',
+                        borderRadius: '24px'
+                    }}
+                > 
+            
+            <NotificationsButton
+                wallet={wallet}
+                network={'mainnet'}
+                publicKey={DIALECT_PUBLIC_KEY}
+                theme={theme}
+                variables={themeVariables}
+                notifications={[
+                    { name: 'Welcome message', detail: 'On thread creation' },
+                ]}
+                channels={['web3', 'email', 'sms', 'telegram']}
+                />
+            <ChatButton wallet={wallet} network={'mainnet'} theme={theme} />
+        </Box>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,191 @@
+const colors = require("tailwindcss/colors");
+
+const primary = {
+  DEFAULT: "#00D18C",
+  50: "#8AFFD8",
+  100: "#75FFD2",
+  200: "#4CFFC4",
+  300: "#24FFB7",
+  400: "#00FAA7",
+  500: "#00D18C",
+  600: "#009966",
+  700: "#006141",
+  800: "#00291B",
+  900: "#000000",
+};
+
+const accent = {
+  DEFAULT: "#9945FF",
+  50: "#FEFDFF",
+  100: "#F2E8FF",
+  200: "#DCBFFF",
+  300: "#C697FF",
+  400: "#AF6EFF",
+  500: "#9945FF",
+  600: "#7A0DFF",
+  700: "#6000D4",
+  800: "#46009C",
+  900: "#2D0064",
+};
+
+const coolGray = {
+  DEFAULT: "#6e7582",
+  50: "#f9fafb",
+  100: "#f0f1f3",
+  150: "#eff1f4",
+  200: "#d9dbdf",
+  300: "#AAB8C1",
+  400: "#9CA3AF",
+  500: "#6B7280",
+  600: "#4B5563",
+  700: "#374151",
+  800: "#161E26",
+  900: "#050505",
+};
+
+const warmGray = {
+  DEFAULT: "#6e7582",
+  50: "#f9fafb",
+  100: "#f0f1f3",
+  150: "#eff1f4",
+  200: "#d9dbdf",
+  300: "#b7bbc2",
+  400: "#8f959f",
+  500: "#6e7582",
+  600: "#555e6e",
+  700: "#3e4859",
+  800: "#222222",
+  850: "#181818",
+  900: "#050505",
+};
+
+const grays = {
+  DEFAULT: "#6e7582",
+  50: "#f9fafb",
+  100: "#f0f1f3",
+  150: "#eff1f4",
+  200: "#d9dbdf",
+  300: "#b7bbc2",
+  400: "#8f959f",
+  500: "#6e7582",
+  600: "#555e6e",
+  700: "#3e4859",
+  800: "#283242",
+  850: "#1f2023",
+  900: "#131f30",
+};
+
+const textColor = {
+  DEFAULT: grays[800],
+  secondary: grays[500],
+};
+
 module.exports = {
-  content: ['./{components,pages}/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{html,ts,tsx}"],
+  darkMode: "class",
+
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1220px",
+    },
     extend: {
+      fontSize: {
+        sm: ".8125rem",
+      },
       fontFamily: {
-        display: ['Lato, sans-serif'],
-        body: ['Lato, sans-serif'],
-        inter: ['Inter, sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
-      minWidth: {
-        0: '0',
-        '1/4': '25%',
-        '1/2': '50%',
-        '3/4': '75%',
-        full: '100%',
-        120: '120px',
+      colors: {
+        ...colors,
+        primary,
+        accent,
+        gray: grays,
+        warmGray,
+        coolGray,
       },
-      backgroundColor: (theme) => ({
-        ...theme('colors'),
-        night: '#252525',
-        // TODO: remove after update to tailwind v3
-        'white/5': 'rgba(255,255,255,.05)',
-        'night/5': 'rgba(37,37,37,.05)',
-      }),
+      textColor,
+      scale: {
+        98: ".98",
+        102: "1.02",
+      },
+      borderColor: {
+        DEFAULT: grays[150],
+      },
+      typography: {
+        sm: {
+          css: {
+            fontSize: ".8125rem",
+            h1: {
+              fontSize: ".8125rem",
+            },
+            h2: {
+              fontSize: ".8125rem",
+            },
+            h3: {
+              fontSize: ".8125rem",
+            },
+          },
+        },
+        DEFAULT: {
+          css: {
+            color: grays[500],
+            strong: {
+              color: grays[800],
+            },
+            h1: {
+              color: grays[800],
+              fontWeight: 500,
+            },
+            h2: {
+              color: grays[800],
+              fontWeight: 500,
+            },
+            h3: {
+              color: grays[800],
+              fontWeight: 500,
+            },
+            code: {
+              color: grays[500],
+            },
+          },
+        },
+        light: {
+          css: {
+            color: grays[300],
+            strong: {
+              color: grays[50],
+            },
+            h1: {
+              color: grays[50],
+              fontWeight: 500,
+            },
+            h2: {
+              color: grays[50],
+              fontWeight: 500,
+            },
+            h3: {
+              color: grays[50],
+              fontWeight: 500,
+            },
+            code: {
+              color: grays[300],
+            },
+          },
+        },
+      },
     },
   },
   variants: {
     extend: {
-      cursor: ['hover', 'focus', 'disabled'],
-      opacity: ['disabled'],
-      backgroundColor: ['disabled'],
-      textColor: ['disabled'],
+      typography: ["dark"],
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
