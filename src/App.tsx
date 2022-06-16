0711367a9e11ecc20250bb1aa28a9e6d18ce8d6a
Filipp Sher <filipp.sher@gmail.com>
2022-06-16 18:36:09 +0400

    chore: reformat, add additional props after dialect version bump, import styles from package

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React, { FC, ReactNode, useCallback, useMemo, Suspense } from 'react';
 import { styled, ThemeProvider } from '@mui/material/styles';
-import { HashRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import { LogView } from "./Log/Log";
-import { ProfileView } from "./Profile/Profile";
-import { PreviewView } from "./Preview/Preview";
-import { FeaturedView } from "./Featured/Featured";
-import { IdentityView } from "./Identity/Identity";
-import { SplashView } from "./Splash/Splash";
-import { StoreFrontView } from "./StoreFront/StoreFront";
-import { MessagesView } from "./Messages/Messages";
+import { ProfileView } from './Profile/Profile';
+import { PreviewView } from './Preview/Preview';
+import { FeaturedView } from './Featured/Featured';
+import { IdentityView } from './Identity/Identity';
+import { SplashView } from './Splash/Splash';
+import { StoreFrontView } from './StoreFront/StoreFront';
+import { MessagesView } from './Messages/Messages';
 //import { NotificationsView } from "./NotificationsTest/NotificationsTest";
 import CssBaseline from '@mui/material/CssBaseline';
 
-import {
-  Box,
-  Grid,
-  Paper,
-  Container,
-  Typography,
-  AppBar,
-} from '@mui/material';
+import '@dialectlabs/react-ui/lib/index.css';
+
+import { Box, Grid, Paper, Container, Typography, AppBar } from '@mui/material';
 
 import Header from './Header/Header';
 import { SnackbarProvider } from 'notistack';
@@ -36,80 +31,82 @@ import { useTranslation } from 'react-i18next';
 
 //import { WalletDialogProvider, WalletDisconnectButton, WalletMultiButton } from '../WalletAdapterMui';
 
-
 import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  GlowWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SlopeWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter,
-  CloverWalletAdapter,
-  MathWalletAdapter,
-  Coin98WalletAdapter,
-  SolongWalletAdapter,
-  BitKeepWalletAdapter,
-  TokenPocketWalletAdapter,
-  BitpieWalletAdapter,
-  SafePalWalletAdapter,
-  ExodusWalletAdapter
+    LedgerWalletAdapter,
+    PhantomWalletAdapter,
+    GlowWalletAdapter,
+    SolflareWalletAdapter,
+    SolletExtensionWalletAdapter,
+    SlopeWalletAdapter,
+    SolletWalletAdapter,
+    TorusWalletAdapter,
+    CloverWalletAdapter,
+    MathWalletAdapter,
+    Coin98WalletAdapter,
+    SolongWalletAdapter,
+    BitKeepWalletAdapter,
+    TokenPocketWalletAdapter,
+    BitpieWalletAdapter,
+    SafePalWalletAdapter,
+    ExodusWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 
 //import { mainListItems, secondaryListItems } from './components/SidebarList/SidebarList';
-import grapeTheme from  './utils/config/theme'
+import grapeTheme from './utils/config/theme';
 //import "./App.less";
 import { GRAPE_RPC_ENDPOINT, TX_RPC_ENDPOINT } from './utils/grapeTools/constants';
+import { DialectUiManagementProvider } from '@dialectlabs/react-ui';
 
 function Copyright(props: any): JSX.Element {
-  const { t, i18n } = useTranslation();
+    const { t, i18n } = useTranslation();
     return (
-    <Typography sx={{background:'transparent'}} variant="body2" color="text.secondary" align="center" {...props}>
-      {t('Powered by Grape on Solana')}
-      {/*
+        <Typography sx={{ background: 'transparent' }} variant="body2" color="text.secondary" align="center" {...props}>
+            {t('Powered by Grape on Solana')}
+            {/*
       <Link color="inherit" href="https://verify.grapes.network">
         Grape Network | Dashboard v1.1.5
       </Link>
       */}
-    </Typography>
-  );
+        </Typography>
+    );
 }
 
-
 function DashboardContent() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
-  // You can also provide a custom RPC endpoint
-  const network = WalletAdapterNetwork.Mainnet; //.Devnet; //.Mainnet;
-  // You can also provide a custom RPC endpoint
-  //const endpoint =  useMemo(() => clusterApiUrl(network), [network]); // GRAPE_RPC_ENDPOINT;
-  //const endpoint =  GRAPE_RPC_ENDPOINT;
-  const endpoint =  TX_RPC_ENDPOINT;
-  const wallets = useMemo(() => [
-    new PhantomWalletAdapter(),
-    new GlowWalletAdapter(),
-    new SolflareWalletAdapter(),
-    new SlopeWalletAdapter(),
-    new LedgerWalletAdapter(),
-    new ExodusWalletAdapter(),
-    new SolletWalletAdapter({ network }),
-    new SolletExtensionWalletAdapter({ network }),
-    new TorusWalletAdapter(),
-    new CloverWalletAdapter(),
-    new MathWalletAdapter(),
-    new Coin98WalletAdapter(),
-    new SolongWalletAdapter(),
-    new BitKeepWalletAdapter(),
-    new TokenPocketWalletAdapter(),
-    new SafePalWalletAdapter(),
-    new BitpieWalletAdapter()
-  ], [network]);
-  
-  /*
+    // You can also provide a custom RPC endpoint
+    const network = WalletAdapterNetwork.Mainnet; //.Devnet; //.Mainnet;
+    // You can also provide a custom RPC endpoint
+    //const endpoint =  useMemo(() => clusterApiUrl(network), [network]); // GRAPE_RPC_ENDPOINT;
+    //const endpoint =  GRAPE_RPC_ENDPOINT;
+    const endpoint = TX_RPC_ENDPOINT;
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new GlowWalletAdapter(),
+            new SolflareWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new LedgerWalletAdapter(),
+            new ExodusWalletAdapter(),
+            new SolletWalletAdapter({ network }),
+            new SolletExtensionWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+            new CloverWalletAdapter(),
+            new MathWalletAdapter(),
+            new Coin98WalletAdapter(),
+            new SolongWalletAdapter(),
+            new BitKeepWalletAdapter(),
+            new TokenPocketWalletAdapter(),
+            new SafePalWalletAdapter(),
+            new BitpieWalletAdapter(),
+        ],
+        [network]
+    );
+
+    /*
   const { enqueueSnackbar } = useSnackbar();
   const onError = useCallback(
       (error) => {
@@ -120,116 +117,116 @@ function DashboardContent() {
   );
   */
 
-  return (
-    <>
-      <Suspense fallback="loading">
-          <ThemeProvider theme={grapeTheme}>
-              <div className="grape-gradient-background">
-              <SnackbarProvider>
-                  <ConnectionProvider endpoint={endpoint}>
-                      <WalletProvider wallets={wallets} autoConnect>
-                      
-                      <Grid 
-                          //color={grapeTheme.palette.primary.light}
-                          sx={{ 
-                            flex: 1
-                          }}>
-                          <CssBaseline />
-                          <Router>
-                          <AppBar position="fixed" color="primary" style={{ background: 'rgba(0,0,0,0.5)' }}>
-                              <Header
-                                  open={open} 
-                                  toggleDrawer={toggleDrawer}
-                              />
-                          </AppBar>
-                              
-                            <Grid
-                              component="main"
-                              sx={{
-                                  mt: 6,
-                                  display: 'flex',
-                                  flexGrow: 1
-                              }}
-                              >
-                              <Container maxWidth="xl" sx={{ mb: 4 }}>
-                                  <ConfirmDialog />
-                                  <Routes>
+    return (
+        <>
+            <Suspense fallback="loading">
+                <DialectUiManagementProvider>
+                    <ThemeProvider theme={grapeTheme}>
+                        <div className="grape-gradient-background">
+                            <SnackbarProvider>
+                                <ConnectionProvider endpoint={endpoint}>
+                                    <WalletProvider wallets={wallets} autoConnect>
+                                        <Grid
+                                            //color={grapeTheme.palette.primary.light}
+                                            sx={{
+                                                flex: 1,
+                                            }}
+                                        >
+                                            <CssBaseline />
+                                            <Router>
+                                                <AppBar
+                                                    position="fixed"
+                                                    color="primary"
+                                                    style={{ background: 'rgba(0,0,0,0.5)' }}
+                                                >
+                                                    <Header open={open} toggleDrawer={toggleDrawer} />
+                                                </AppBar>
 
-                                    <Route path="/splash" element={<SplashView/>} />
+                                                <Grid
+                                                    component="main"
+                                                    sx={{
+                                                        mt: 6,
+                                                        display: 'flex',
+                                                        flexGrow: 1,
+                                                    }}
+                                                >
+                                                    <Container maxWidth="xl" sx={{ mb: 4 }}>
+                                                        <ConfirmDialog />
+                                                        <Routes>
+                                                            <Route path="/splash" element={<SplashView />} />
 
-                                    <Route path="/featured" element={<FeaturedView/>} />
-                                    
-                                    <Route path="/messages" element={<MessagesView/>} />
+                                                            <Route path="/featured" element={<FeaturedView />} />
 
-                                    {/*<Route path="/solflaretest" element={<NotificationsView/>} />*/}
-                                    
-                                    <Route path="/" element={<ProfileView/>}>
-                                      <Route path=":handlekey" element={<ProfileView />} />
-                                    </Route>
-                                    <Route index element={<ProfileView/>} />
-                                    
-                                    <Route path="store/*" element={<StoreFrontView />} >
-                                        <Route path=":handlekey" element={<StoreFrontView />} />
-                                    </Route>
-                                    <Route path="collection/*" element={<StoreFrontView />} >
-                                        <Route path=":handlekey" element={<StoreFrontView />} />
-                                    </Route>
+                                                            <Route path="/messages" element={<MessagesView />} />
 
-                                    <Route path="profile/*" element={<ProfileView />} >
-                                        <Route path=":handlekey" element={<ProfileView />} />
-                                    </Route>
-                                    
-                                    <Route path="preview/*" element={<PreviewView />}>
-                                        <Route path=":handlekey" element={<PreviewView />} />
-                                    </Route>
+                                                            {/*<Route path="/solflaretest" element={<NotificationsView/>} />*/}
 
-                                    <Route path="identity/*" element={<IdentityView />}>
-                                        <Route path=":handlekey" element={<IdentityView />} />
-                                    </Route>
+                                                            <Route path="/" element={<ProfileView />}>
+                                                                <Route path=":handlekey" element={<ProfileView />} />
+                                                            </Route>
+                                                            <Route index element={<ProfileView />} />
 
-                                    <Route path="*" element={<NotFound />} />
-                                  </Routes>
-                                  
-                                  <Copyright sx={{ mt: 4 }} />
-                              </Container>
-                            </Grid>
-                          </Router>
-                      </Grid>
-                      
-                      </WalletProvider>
-                  </ConnectionProvider>
-              </SnackbarProvider>
-              </div>
-          </ThemeProvider>
-        </Suspense>
-    </>
-  );
+                                                            <Route path="store/*" element={<StoreFrontView />}>
+                                                                <Route path=":handlekey" element={<StoreFrontView />} />
+                                                            </Route>
+                                                            <Route path="collection/*" element={<StoreFrontView />}>
+                                                                <Route path=":handlekey" element={<StoreFrontView />} />
+                                                            </Route>
+
+                                                            <Route path="profile/*" element={<ProfileView />}>
+                                                                <Route path=":handlekey" element={<ProfileView />} />
+                                                            </Route>
+
+                                                            <Route path="preview/*" element={<PreviewView />}>
+                                                                <Route path=":handlekey" element={<PreviewView />} />
+                                                            </Route>
+
+                                                            <Route path="identity/*" element={<IdentityView />}>
+                                                                <Route path=":handlekey" element={<IdentityView />} />
+                                                            </Route>
+
+                                                            <Route path="*" element={<NotFound />} />
+                                                        </Routes>
+
+                                                        <Copyright sx={{ mt: 4 }} />
+                                                    </Container>
+                                                </Grid>
+                                            </Router>
+                                        </Grid>
+                                    </WalletProvider>
+                                </ConnectionProvider>
+                            </SnackbarProvider>
+                        </div>
+                    </ThemeProvider>
+                </DialectUiManagementProvider>
+            </Suspense>
+        </>
+    );
 }
 
 export const NotFound = () => {
-  return (
-    <div style={{ height: "100%", overflow: "auto" }}>
-      <Paper className="grape-paper-background">
-        <Grid 
-          className="grape-paper" 
-          container
-          alignContent="center"
-          justifyContent="center"
-          direction="column">
-          <Grid item>
-            <Typography 
-              align="center"
-              variant="h3">
-              {'No Grapes Here...'}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Paper>
-  </div>
-  )
-}
+    return (
+        <div style={{ height: '100%', overflow: 'auto' }}>
+            <Paper className="grape-paper-background">
+                <Grid
+                    className="grape-paper"
+                    container
+                    alignContent="center"
+                    justifyContent="center"
+                    direction="column"
+                >
+                    <Grid item>
+                        <Typography align="center" variant="h3">
+                            {'No Grapes Here...'}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </div>
+    );
+};
 
 //export const Dashboard: FC<{ children: ReactNode }> = ({ children }) => {
 export default function Dashboard() {
-  return <DashboardContent />;
+    return <DashboardContent />;
 }
--- src/Messages/Messages.tsx
+++ src/Messages/Messages.tsx
@@ -1,100 +1,82 @@
 import React from 'react';
-import '../dialect.css';
-//import '@dialectlabs/react-ui/lib/index.css';
 import {
     ChatButton,
     NotificationsButton,
     IncomingThemeVariables,
     defaultVariables,
-    Inbox as DialectInbox, 
-    ThemeProvider
-  } from '@dialectlabs/react-ui';
-import {
-    ApiProvider,
-    connected,
-    DialectProvider,
-    useApi,
-} from '@dialectlabs/react';
+    Inbox as DialectInbox,
+    ThemeProvider,
+} from '@dialectlabs/react-ui';
+import { ApiProvider, connected, DialectProvider, useApi } from '@dialectlabs/react';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { PublicKey } from '@solana/web3.js';
 
 import { GRAPE_RPC_ENDPOINT, TX_RPC_ENDPOINT, GENSYSGO_RPC_ENDPOINT } from '../utils/grapeTools/constants';
 
-import {
-    Box,
-  } from '@mui/material';
-  import {
-    AUCTION_HOUSE_PROGRAM_ID,
-    AUCTION_HOUSE_ADDRESS,
-  } from '../utils/auctionHouse/helpers/constants';
+import { Box } from '@mui/material';
+import { AUCTION_HOUSE_PROGRAM_ID, AUCTION_HOUSE_ADDRESS } from '../utils/auctionHouse/helpers/constants';
 
-  export const themeVariables: IncomingThemeVariables = {
+export const themeVariables: IncomingThemeVariables = {
     dark: {
-      bellButton:
-        'w-10 h-10 shadow-xl shadow-neutral-800 border border-neutral-600 hover:shadow-neutral-700 bg-white text-black',
-      modal: `${defaultVariables.dark.modal} sm:rounded-3xl shadow-xl shadow-neutral-900 sm:border border-[#ABABAB]/40`, // 0.4 opacity based on trial-and-error
+        bellButton:
+            'w-10 h-10 shadow-xl shadow-neutral-800 border border-neutral-600 hover:shadow-neutral-700 bg-white text-black',
+        modal: `${defaultVariables.dark.modal} sm:rounded-3xl shadow-xl shadow-neutral-900 sm:border border-[#ABABAB]/40`, // 0.4 opacity based on trial-and-error
     },
     animations: {
-      popup: {
-        enter: 'transition-all duration-300 origin-top-right',
-        enterFrom: 'opacity-0 scale-75',
-        enterTo: 'opacity-100 scale-100',
-        leave: 'transition-all duration-100 origin-top-right',
-        leaveFrom: 'opacity-100 scale-100',
-        leaveTo: 'opacity-0 scale-75',
-      },
+        popup: {
+            enter: 'transition-all duration-300 origin-top-right',
+            enterFrom: 'opacity-0 scale-75',
+            enterTo: 'opacity-100 scale-100',
+            leave: 'transition-all duration-100 origin-top-right',
+            leaveFrom: 'opacity-100 scale-100',
+            leaveTo: 'opacity-0 scale-75',
+        },
     },
-  };
+};
 
-  function AuthedHome() {
+function AuthedHome() {
     const wallet = useWallet();
     const isWalletConnected = connected(wallet);
-    
 
     const { setNetwork, setRpcUrl, setWallet } = useApi();
-  
-    React.useEffect(
-      () => setWallet(connected(wallet) ? wallet : null),
-      [setWallet, wallet, isWalletConnected]
-    );
+
+    React.useEffect(() => setWallet(connected(wallet) ? wallet : null), [setWallet, wallet, isWalletConnected]);
     React.useEffect(() => setNetwork('mainnet'), [setNetwork]);
     React.useEffect(() => setRpcUrl(null), [setRpcUrl]);
-  
-    return (
-      <div className="dialect">
-        <div className="flex flex-col h-screen bg-black">
-          <div className="w-full lg:max-w-[1048px] px-6 h-[calc(100vh-8rem)] mt-8 mx-auto">
-            <DialectInbox
-              wrapperClassName="p-2 h-full overflow-hidden rounded-2xl shadow-2xl shadow-neutral-800 border border-neutral-600"
-              wallet={wallet}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
 
-export function MessagesView(){
+    return (
+        <div className="dialect">
+            <div className="flex flex-col h-screen bg-black">
+                <div className="w-full lg:max-w-[1048px] px-6 h-[calc(100vh-8rem)] mt-8 mx-auto">
+                    <DialectInbox
+                        dialectId="grape-inbox"
+                        wrapperClassName="p-2 h-full overflow-hidden rounded-2xl shadow-2xl shadow-neutral-800 border border-neutral-600"
+                        wallet={wallet}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function MessagesView() {
     // ...
     const wallet = useWallet();
     const theme: 'dark' | 'light' = 'dark';
     //const YOUR_PROJECT_PUBLIC_KEY = new PublicKey(AUCTION_HOUSE_ADDRESS);
-    const DIALECT_PUBLIC_KEY = new PublicKey(
-        'D2pyBevYb6dit1oCx6e8vCxFK9mBeYCRe8TTntk2Tm98'
-    );
-    
+    const DIALECT_PUBLIC_KEY = new PublicKey('D2pyBevYb6dit1oCx6e8vCxFK9mBeYCRe8TTntk2Tm98');
+
     return (
         <Box
-            sx={{ 
-                p: 1, 
-                mt: 6, 
-                mb: 3, 
+            sx={{
+                p: 1,
+                mt: 6,
+                mb: 3,
                 width: '100%',
                 background: '#13151C',
-                borderRadius: '24px'
+                borderRadius: '24px',
             }}
-        > 
-            
+        >
             <NotificationsButton
                 wallet={wallet}
                 network={'mainnet'}
@@ -102,15 +84,10 @@ export function MessagesView(){
                 theme={theme}
                 variables={themeVariables}
                 rpcUrl={GENSYSGO_RPC_ENDPOINT}
-                notifications={[
-                    { name: 'Welcome message', detail: 'On thread creation' },
-                ]}
+                notifications={[{ name: 'Welcome message', detail: 'On thread creation' }]}
                 channels={['web3', 'email', 'sms', 'telegram']}
-                />
+            />
             <ChatButton wallet={wallet} network={'mainnet'} theme={theme} rpcUrl={GENSYSGO_RPC_ENDPOINT} />
         </Box>
-
-
-
     );
 }
