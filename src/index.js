0948854f624be46dc8cf62f7856853f8b7286254
Kirk <kfournia@gmail.com>
2022-06-17 12:11:17 +0300

    card rendering

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import i18n (needs to be bundled) 
 import './i18n';
+
+/*
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}*/
 
 ReactDOM.render(
   <React.StrictMode>
