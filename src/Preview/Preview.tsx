51a408abffaf889d18ce0c5bc553bece16791769
Kirk <kfournia@gmail.com>
2022-07-20 10:21:18 +0300

    added additional styling to preview page

--- src/Preview/Preview.tsx
+++ src/Preview/Preview.tsx
@@ -944,6 +944,10 @@ function GalleryItemMeta(props: any) {
                                 <style>{'html, body { background: '+verifiedCollection.theme+' fixed!important;height: 100%; }'}</style>
                             }
 
+                            {verifiedCollection.theme && verifiedCollection.themeImage &&
+                                <style>{'html, body { background: '+verifiedCollection.theme+' fixed!important;height: 100%; background-image:'+verifiedCollection.themeImage+'!important; background-size: 10px 10px!important; }'}</style>
+                            }
+
                         </Helmet>
                     </>
                 }
