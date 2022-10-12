f222caa6bfb8e97440ac324877e1f52dc15b3803
Kirk <kfournia@gmail.com>
2022-10-12 23:43:56 +0300

    version bump

--- src/StoreFront/Governance.tsx
+++ src/StoreFront/Governance.tsx
@@ -307,19 +307,19 @@ function RenderGovernanceTable(props:any) {
                     if (counter > 1)
                         csvFile += ',';
                     else
-                        csvFile += 'tokenOwner,voterWeight,tokenDecimals,voteType,';
+                        csvFile += '[tokenOwner,voterWeight,tokenDecimals,voteType],';
                     
-                    csvFile += item.pubkey.toBase58()+','+item.account.voterWeight.toNumber()+','+thisToken?.decimals+','+item.account.vote.voteType;
+                    csvFile += '['+item.pubkey.toBase58()+','+item.account.voterWeight.toNumber()+','+thisToken?.decimals+','+item.account.vote.voteType+']';
                     
                     /*
                     const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
                         JSON.stringify(votingResults)
                     )}`;
                     */
-                    const jsonCSVString = `data:text/csv;chatset=utf-8,${csvFile}`;
+                    const jsonCSVString = `data:text/csv;chatset=utf-8,${'['+csvFile+']'}`;
                     console.log("jsonCSVString: "+JSON.stringify(jsonCSVString));
-                    
-                    //setCSVGenerated(jsonCSVString); 
+
+                    setCSVGenerated(jsonCSVString); 
                     
                 }
             }
