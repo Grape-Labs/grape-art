00aca0cab696228fbf208fbcc858637eebb42833
Kirk <kfournia@gmail.com>
2022-10-22 17:40:24 +0300

    governance fields added for proposal details prompt

--- src/StoreFront/Governance.tsx
+++ src/StoreFront/Governance.tsx
@@ -501,12 +501,12 @@ function RenderGovernanceTable(props:any) {
                                             <Typography variant="subtitle2">
                                                 <Chip
                                                     variant="outlined"
-                                                    label={moment.unix(thisitem.account?.votingAt.toNumber()).format("MMMM Da, YYYY, h:mm a")}
+                                                    label={moment.unix(thisitem?.votingAt.toNumber()).format("MMMM Da, YYYY, h:mm a")}
                                                 />
                                             </Typography>
                                         </Box>
                                     </Grid>
-
+                                    
                                     <Grid item xs={12} sm={6} md={3} key={1}>
                                         <Box
                                             className='grape-store-stat-item'
