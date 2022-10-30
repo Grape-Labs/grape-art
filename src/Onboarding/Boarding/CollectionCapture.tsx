f8725acce2b4b8a75ffad781202a113e91820c42
Fuzzy Yeti <fuzzyyeti@github.com>
2022-10-29 23:56:51 -0600

    cleanup

--- src/Onboarding/Boarding/CollectionCapture.tsx
+++ src/Onboarding/Boarding/CollectionCapture.tsx
@@ -1,135 +1,82 @@
-import React, { useEffect, useState, useCallback, memo, Suspense } from "react";
+import React from 'react';
 
-import {
-    Box,
-    TextField,
-    Button,
-    LinearProgress,
-    Typography,
-    Stack
-} from '@mui/material';
+import { TextField, Stack } from '@mui/material';
 
-import { PublicKey } from "@solana/web3.js"
+import { PublicKey } from '@solana/web3.js';
 
-import { LinearProgressProps } from '@mui/material/LinearProgress';
-
-export function CollectionCaptureView (this: any, props: {
-        setGovernance: (governance: PublicKey) => void,
-        setName: (name: string ) => void,
-        setVanityUrl: (url: string) => void,
-        setMetaDataUrl: (url: string) => void,
-        setVerifiedCollectionAddress: (address: PublicKey) => void
-        setUpdateAuthority: (address: PublicKey) => void
-        setAuctionHouse: (address: PublicKey) => void
-        setCreatorAddress: (address: PublicKey) => void
-        setTokenType: (type: string) => void
-    }) {
-    const [collectionAddress, setCollectionAddress] = React.useState(null);
-    const [updateAuthorityAddress, setUpdateAuthorityAddress] = React.useState(null);
-    const [progress, setProgress] = React.useState(0);
-    const [status, setStatus] = React.useState(null);
-    const [MAX, setMax] = React.useState(100);
-    const MIN = 0;
-
+export function CollectionCaptureView(
+    this: any,
+    props: {
+        setGovernance: (governance: PublicKey) => void;
+        setName: (name: string) => void;
+        setVanityUrl: (url: string) => void;
+        setMetaDataUrl: (url: string) => void;
+        setVerifiedCollectionAddress: (address: PublicKey) => void;
+        setUpdateAuthority: (address: PublicKey) => void;
+        setAuctionHouse: (address: PublicKey) => void;
+        setCreatorAddress: (address: PublicKey) => void;
+        setTokenType: (type: string) => void;
+    }
+) {
     return (
-        
-            <Stack
-                component="form"
-                m={2}
-                sx={{
-                    width: '25ch',
-                }}
-                spacing={2}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField 
-                    fullWidth 
-                    label="Collection Name" 
-                    onChange={(e) => props.setName(e.target.value)}
-                />
-                <TextField 
-                    fullWidth 
-                    label="Author (leave blank if the name is the same as the collection name)" 
-                    //onChange={(e) => setUpdateAuthorityAddress(e.target.value)}
-                />
-                <TextField 
-                    fullWidth 
-                    label="Description" 
-                    //onChange={(e) => setUpdateAuthorityAddress(e.target.value)}
-                />
+        <Stack
+            component="form"
+            m={2}
+            sx={{
+                width: '25ch',
+            }}
+            spacing={2}
+            noValidate
+            autoComplete="off"
+        >
+            <TextField fullWidth label="Collection Name" onChange={(e) => props.setName(e.target.value)} />
+            <TextField
+                fullWidth
+                label="Author (leave blank if the name is the same as the collection name)"
+                //TODO: send to metadata
+            />
+            <TextField
+                fullWidth
+                label="Description"
+                //TODO: send to metadata
+            />
 
-                <TextField 
-                    fullWidth 
-                    label="Meta-data URL"
-                    onChange={(e)=> props.setMetaDataUrl(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="Vanity URL"
-                    onChange={(e)=> props.setVanityUrl(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="Token Type"
-                    onChange={(e)=>props.setTokenType(e.target.value)}
-                />
-                {/*
-                <TextField 
-                    fullWidth 
-                    label="Discord" 
-                    //onChange={(e) => setUpdateAuthorityAddress(e.target.value)}
-                />
+            <TextField fullWidth label="Meta-data URL" onChange={(e) => props.setMetaDataUrl(e.target.value)} />
+            <TextField fullWidth label="Vanity URL" onChange={(e) => props.setVanityUrl(e.target.value)} />
+            <TextField fullWidth label="Token Type" onChange={(e) => props.setTokenType(e.target.value)} />
+            <TextField
+                fullWidth
+                label="Governance Address"
+                onChange={(e) => props.setGovernance(new PublicKey(e.target.value))}
+            />
+            <TextField
+                fullWidth
+                label="Auction House"
+                onChange={(e) => props.setAuctionHouse(new PublicKey(e.target.value))}
+            />
+            <TextField
+                fullWidth
+                label="Update Authority"
+                onChange={(e) => props.setUpdateAuthority(new PublicKey(e.target.value))}
+            />
 
-                <TextField 
-                    fullWidth 
-                    label="Twitter" 
-                    //onChange={(e) => setUpdateAuthorityAddress(e.target.value)}
-                />
-                */}
+            <TextField
+                fullWidth
+                label="Verified Collection Address"
+                onChange={(e) => props.setVerifiedCollectionAddress(new PublicKey(e.target.value))}
+            />
 
-                <TextField 
-                    fullWidth 
-                    label="Governance Address"
-                    onChange={(e) =>
-                        props.setGovernance( new PublicKey(e.target.value))}
-                />
-                <TextField
-                    fullWidth
-                    label="Auction House"
-                    onChange={(e) =>
-                        props.setAuctionHouse( new PublicKey(e.target.value))}
-                />
-                <TextField
-                    fullWidth
-                    label="Update Authority"
-                    onChange={(e) =>
-                        props.setUpdateAuthority( new PublicKey(e.target.value))}
-                />                
-                
-                <TextField
-                    fullWidth
-                    label="Verified Collection Address"
-                    onChange={(e) =>
-                        props.setVerifiedCollectionAddress( new PublicKey(e.target.value))}
-                />
+            <TextField
+                fullWidth
+                label="Creator Address (for editions)"
+                onChange={(e) => props.setCreatorAddress(new PublicKey(e.target.value))}
+            />
 
-                <TextField
-                    fullWidth
-                    label="Creator Address (for editions)"
-                    onChange={(e) =>
-                        props.setCreatorAddress( new PublicKey(e.target.value))}
-                />
-
-                <TextField 
-                    fullWidth 
-                    label="Random Mint Sample"
-                    //onChange={(e) => setUpdateAuthorityAddress(e.target.value)}
-                />
-
-
-                
-            </Stack>
-            
+            <TextField
+                fullWidth
+                label="Random Mint Sample"
+                //TODO: send to metadata
+            />
+        </Stack>
     );
 }
--- src/Onboarding/MyCollections/PendingTable.tsx
+++ src/Onboarding/MyCollections/PendingTable.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {CollectionBoardingInfo} from "grape-art-listing-request";
+import { CollectionBoardingInfo } from 'grape-art-listing-request';
 import {
     Paper,
     TableContainer,
@@ -10,84 +10,33 @@ import {
     Table,
     TableBody,
     Button,
-    Box,
-} from "@mui/material";
-import { styled, useTheme } from '@mui/material/styles';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import {PublicKey} from "@solana/web3.js";
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { PublicKey } from '@solana/web3.js';
 
 const StyledTable = styled(Table)(({ theme }) => ({
     '& .MuiTableCell-root': {
-        borderBottom: '1px solid rgba(255,255,255,0.05)'
+        borderBottom: '1px solid rgba(255,255,255,0.05)',
     },
 }));
 
-export function PendingTable({info, isAdmin, approve, wallet, refund}: {
-    info: CollectionBoardingInfo[] | undefined,
-    isAdmin: boolean,
-    approve: (seed: PublicKey) => Promise<string>,
-    wallet: PublicKey,
-    refund: (seed: PublicKey) => Promise<string>})  {
-
-    const boardingcols = [
-        { field: 'collectionName', headerName: 'Collection Name', width: 70, flex: 1 },
-        { field: 'listingRequestor', headerName: 'Listing Requestor', width: 50},
-        { field: 'updateAuthority', headerName: 'Update Authority', width: 50}
-    ];
-    
-    const [boardingRows, setBoardingRows] = React.useState(null);
-
-    
-    
-    React.useEffect(() => {
-        const theseRows = new Array();
-        for (var listing of info){
-            theseRows.push({
-                collectionName: listing.name,
-                listingRequestor: listing.listing_requester.toBase58(),
-                updateAuthority: listing.collection_update_authority.toBase58()
-            });
-        }
-        setBoardingRows(theseRows);
-    }, [info]);
-    
-
-
-    return (<div>
-            
-            {/*boardingRows && boardingRows.length > 0 &&
-                <Box sx={{mt:2,p:1}}>
-                    <div style={{ height: 600, width: '100%' }}>
-                        <div style={{ display: 'flex', height: '100%' }}>
-                            <div style={{ flexGrow: 1 }}>
-                                
-                                {JSON.stringify(boardingRows)}
-                                {
-                                <DataGrid
-                                    rows={boardingRows}
-                                    columns={boardingcols}
-                                    rowsPerPageOptions={[25, 50, 100, 250]}
-                                    sx={{
-                                        borderRadius:'17px',
-                                        borderColor:'rgba(255,255,255,0.25)',
-                                        '& .MuiDataGrid-cell':{
-                                            borderColor:'rgba(255,255,255,0.25)'
-                                        }}}
-                                    sortingOrder={['asc', 'desc', null]}
-                                    //checkboxSelection
-                                    disableSelectionOnClick
-                                />
-                                }
-                                
-                            </div>
-                        </div>
-                    </div>
-                    
-                </Box>
-            */}
-
+export function PendingTable({
+    info,
+    isAdmin,
+    approve,
+    wallet,
+    refund,
+}: {
+    info: CollectionBoardingInfo[] | undefined;
+    isAdmin: boolean;
+    approve: (seed: PublicKey) => Promise<string>;
+    wallet: PublicKey;
+    refund: (seed: PublicKey) => Promise<string>;
+}) {
+    return (
+        <div>
             <Table>
-                <TableContainer component={Paper} sx={{background:'none'}}>
+                <TableContainer component={Paper} sx={{ background: 'none' }}>
                     <StyledTable sx={{ minWidth: 500 }} size="small" aria-label="Approved Collections Table">
                         <TableHead sx={{ backgroundColor: 'inherit' }}>
                             <TableRow sx={{ backgroundColor: 'inherit' }}>
@@ -98,39 +47,77 @@ export function PendingTable({info, isAdmin, approve, wallet, refund}: {
                                     <Typography variant="caption">Listing Requestor</Typography>
                                 </TableCell>
                                 <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption" sx={{ marginRight: '30px' }}>Update Authority</Typography>
+                                    <Typography variant="caption" sx={{ marginRight: '30px' }}>
+                                        Update Authority
+                                    </Typography>
                                 </TableCell>
-                                {isAdmin && <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption" sx={{ marginRight: '30px' }}>Approve</Typography>
-                                </TableCell>}
-                                {info.find((listing) => listing.listing_requester.equals(wallet)) &&  <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption" sx={{ marginRight: '30px' }}>Refund</Typography>
-                                </TableCell>}
+                                {isAdmin && (
+                                    <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                        <Typography variant="caption" sx={{ marginRight: '30px' }}>
+                                            Approve
+                                        </Typography>
+                                    </TableCell>
+                                )}
+                                {info.find((listing) => listing.listing_requester.equals(wallet)) && (
+                                    <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                        <Typography variant="caption" sx={{ marginRight: '30px' }}>
+                                            Refund
+                                        </Typography>
+                                    </TableCell>
+                                )}
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {info && info.map((listing) =>
-                            <TableRow sx={{ backgroundColor: 'inherit' }}>
-                                <TableCell sx={{ minWidth: '180px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption">{listing.name}</Typography>
-                                </TableCell>
-                                <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption">{listing.listing_requester.toBase58()}</Typography>
-                                </TableCell>
-                                <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Typography variant="caption" sx={{ marginRight: '30px' }}>{listing.collection_update_authority.toBase58()}</Typography>
-                                </TableCell>
-                                {isAdmin && <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Button onClick={() => approve(listing.verified_collection_address || listing.collection_update_authority)}>Approve</Button>
-                                </TableCell>}
-                                {listing.listing_requester.equals(wallet) && <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
-                                    <Button onClick={() => refund(listing.verified_collection_address || listing.collection_update_authority)}>Request Refund</Button>
-                                    </TableCell>}
-                            </TableRow>
-                            )}
+                            {info &&
+                                info.map((listing) => (
+                                    <TableRow sx={{ backgroundColor: 'inherit' }}>
+                                        <TableCell sx={{ minWidth: '180px', backgroundColor: 'inherit' }}>
+                                            <Typography variant="caption">{listing.name}</Typography>
+                                        </TableCell>
+                                        <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                            <Typography variant="caption">
+                                                {listing.listing_requester.toBase58()}
+                                            </Typography>
+                                        </TableCell>
+                                        <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                            <Typography variant="caption" sx={{ marginRight: '30px' }}>
+                                                {listing.collection_update_authority.toBase58()}
+                                            </Typography>
+                                        </TableCell>
+                                        {isAdmin && (
+                                            <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                                <Button
+                                                    onClick={() =>
+                                                        approve(
+                                                            listing.verified_collection_address ||
+                                                                listing.collection_update_authority
+                                                        )
+                                                    }
+                                                >
+                                                    Approve
+                                                </Button>
+                                            </TableCell>
+                                        )}
+                                        {listing.listing_requester.equals(wallet) && (
+                                            <TableCell sx={{ minWidth: '100px', backgroundColor: 'inherit' }}>
+                                                <Button
+                                                    onClick={() =>
+                                                        refund(
+                                                            listing.verified_collection_address ||
+                                                                listing.collection_update_authority
+                                                        )
+                                                    }
+                                                >
+                                                    Request Refund
+                                                </Button>
+                                            </TableCell>
+                                        )}
+                                    </TableRow>
+                                ))}
                         </TableBody>
                     </StyledTable>
-                    </TableContainer>
-                </Table>
-    </div>)
+                </TableContainer>
+            </Table>
+        </div>
+    );
 }
