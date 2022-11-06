8530a6490a241ad9d62f8bdc9591a81c8d30394a
Kirk <kfournia@gmail.com>
2022-11-06 11:12:33 +0200

    version bump

--- src/Identity/Identity.tsx
+++ src/Identity/Identity.tsx
@@ -11,16 +11,8 @@ import axios from "axios";
 
 import { getRealm, 
     getRealms, 
-    getAllProposals, 
-    getGovernance, 
     getTokenOwnerRecordsByOwner,
     getTokenOwnerRecord, 
-    getRealmConfigAddress, 
-    getGovernanceAccount, 
-    getAccountTypes, 
-    GovernanceAccountType, 
-    tryGetRealmConfig,
-    GovernanceInstruction,
 } from '@solana/spl-governance';
 //import { ShdwDrive } from "@shadow-drive/sdk";
 
@@ -31,6 +23,7 @@ import { programs, tryGetAccount, withSend, findAta } from '@cardinal/token-mana
 
 import GovernanceDetailsView from './plugins/GovernanceDetails';
 import { SquadsView } from './plugins/Squads';
+import { GovernanceView } from './plugins/Governance';
 import { StorageView } from './plugins/Storage';
 import { StreamingPaymentsView } from './plugins/StreamingPayments';
 import SendToken from '../StoreFront/Send';
@@ -168,18 +161,16 @@ export function IdentityView(props: any){
     const [solanaHoldingRows, setSolanaHoldingRows] = React.useState(null);
     const [solanaClosableHoldings, setSolanaClosableHoldings] = React.useState(null);
     const [solanaClosableHoldingsRows, setSolanaClosableHoldingsRows] = React.useState(null);
-    const [governanceRecord, setGovernanceRecord] = React.useState(null);
-    const [governanceRecordRows, setGovernanceRecordRows] = React.useState(null);
     const [solanaBalance, setSolanaBalance] = React.useState(null);
     const [solanaTransactions, setSolanaTransactions] = React.useState(null);
     const [loadingWallet, setLoadingWallet] = React.useState(false);
     const [loadingTokens, setLoadingTokens] = React.useState(false);
     const [loadingTransactions, setLoadingTransactions] = React.useState(false);
-    const [loadingGovernance, setLoadingGovernance] = React.useState(false);
+    
     const [loadingStorage, setLoadingStorage] = React.useState(false);
     const [loadingStreamingPayments, setLoadingStreamingPayments] = React.useState(false);
     const [loadingPosition, setLoadingPosition] = React.useState('');
-    const [realms, setRealms] = React.useState(null);
+    
     const { publicKey } = useWallet();
     const [pubkey, setPubkey] = React.useState(props.pubkey || null);
     const ggoconnection = new Connection(GRAPE_RPC_ENDPOINT);
@@ -192,7 +183,6 @@ export function IdentityView(props: any){
     const [nftMap, setNftMap] = React.useState(null);
     const [selectionModel, setSelectionModel] = React.useState([]);
     const [selectionModelClose, setSelectionModelClose] = React.useState([]);
-    const [selectionGovernanceModel, setSelectionGovernanceModel] = React.useState(null);
     
     const { t, i18n } = useTranslation();
 
@@ -381,37 +371,7 @@ export function IdentityView(props: any){
         },
       ];
       
-      const governancecolumns: GridColDef[] = [
-        { field: 'id', headerName: 'ID', width: 70, hide: true },
-        { field: 'pubkey', headerName: 'PublicKey', width: 70, hide: true },
-        { field: 'realm', headerName: 'Realm', minWidth: 130, flex: 1, align: 'left' },
-        { field: 'governingTokenMint', headerName: 'Governing Mint', width: 150, align: 'center'},
-        { field: 'governingTokenDepositAmount', headerName: 'Votes', width: 130, align: 'center'},
-        { field: 'unrelinquishedVotesCount', headerName: 'Unreliquinshed', width: 130, align: 'center'},
-        { field: 'totalVotesCount', headerName: 'Total Votes', width: 130, align: 'center' },
-        { field: 'details', headerName: '', width: 150,  align: 'center',
-            renderCell: (params) => {
-                return (
-                    <><GovernanceDetailsView governanceToken={params.value}/></>
-                )
-            }
-        },
-        { field: 'link', headerName: '', width: 150,  align: 'center',
-            renderCell: (params) => {
-                return (
-                    <Button
-                        variant='outlined'
-                        size='small'
-                        component='a'
-                        href={`https://realms.today/dao/${params.value}`}
-                        target='_blank'
-                        sx={{borderRadius:'17px'}}
-                    >Visit</Button>
-                )
-            }
-        },
-      ];
-
+      
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
@@ -1026,55 +986,6 @@ export function IdentityView(props: any){
         }
     }
 
-    const fetchGovernance = async () => {
-        setLoadingPosition('Governance');
-        const GOVERNANCE_PROGRAM_ID = 'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw';
-        const programId = new PublicKey(GOVERNANCE_PROGRAM_ID);
-        
-        const rlms = await getRealms(ticonnection, programId);
-        const uTable = rlms.reduce((acc, it) => (acc[it.pubkey.toBase58()] = it, acc), {})
-        setRealms(uTable);
-        
-        const ownerRecordsbyOwner = await getTokenOwnerRecordsByOwner(ticonnection, programId, new PublicKey(pubkey));
-
-        console.log("ownerRecordsbyOwner "+JSON.stringify(ownerRecordsbyOwner))
-        const governance: any[] = [];
-        
-        let cnt = 0;
-        //console.log("all uTable "+JSON.stringify(uTable))
-
-        for (const item of ownerRecordsbyOwner){
-            const realm = uTable[item.account.realm.toBase58()];
-            //console.log("realm: "+JSON.stringify(realm))
-            const name = realm.account.name;
-            let votes = item.account.governingTokenDepositAmount.toString();
-            
-            let thisToken = tokenMap.get(item.account.governingTokenMint.toBase58());
-            
-            if (thisToken){
-                votes = (new TokenAmount(+item.account.governingTokenDepositAmount, thisToken.decimals).format())
-            } else{
-                votes = 'NFT/Council';
-            }
-
-            governance.push({
-                id:cnt,
-                pubkey:item.pubkey,
-                realm:name,
-                governingTokenMint:item.account.governingTokenMint,
-                governingTokenDepositAmount:votes,
-                unrelinquishedVotesCount:item.account.unrelinquishedVotesCount,
-                totalVotesCount:item.account.totalVotesCount,
-                details:item.account.realm.toBase58(),
-                link:item.account.realm
-            });
-            cnt++;
-        }
-
-        setGovernanceRecord(ownerRecordsbyOwner);
-        setGovernanceRecordRows(governance);
-    }
-
     React.useEffect(() => {
         if (urlParams){
             if (!pubkey){
@@ -1094,25 +1005,12 @@ export function IdentityView(props: any){
     }
     */
 
-    const fetchGovernancePositions = async () => {
-        setLoadingGovernance(true);
-        await fetchGovernance();
-        setLoadingGovernance(false);
-    }
-
     const fetchTokenPositions = async () => {
         setLoadingTokens(true);
         await fetchSolanaTokens();
         await fetchSolanaTransactions();
         setLoadingTokens(false);
     }
-
-    React.useEffect(() => {
-        if (pubkey && tokenMap){
-            fetchTokenPositions();
-            fetchGovernancePositions();
-        }
-    }, [tokenMap]);
 
     const fetchWalletPositions = async () => {
         setLoadingWallet(true);
@@ -1123,6 +1021,12 @@ export function IdentityView(props: any){
         //await fetchStorage();
         setLoadingWallet(false);
     }
+
+    React.useEffect(() => {
+        if (pubkey && tokenMap){
+            fetchTokenPositions();
+        }
+    }, [tokenMap]);
 
     React.useEffect(() => {
         if (pubkey){
@@ -1222,7 +1126,7 @@ export function IdentityView(props: any){
                                             </ListItem>
                                         </List>
                                         
-                                        {(loadingWallet || loadingTokens || loadingGovernance || loadingStorage || loadingStreamingPayments) &&
+                                        {(loadingWallet || loadingTokens || loadingStorage || loadingStreamingPayments) &&
                                             <Grid container spacing={0} sx={{}}>
                                                 {/*
                                                 a. {JSON.stringify(loadingWallet)}
@@ -1266,12 +1170,11 @@ export function IdentityView(props: any){
                                                                 label={<Hidden smDown><Badge badgeContent={solanaClosableHoldings.length} color="error"><Typography variant="h6">{t('Closable')}</Typography></Badge></Hidden>
                                                             } value="3" />
                                                         }
-                                                        {governanceRecord &&
-                                                            <Tab sx={{color:'white', textTransform:'none'}} 
-                                                                icon={<Hidden smUp><Badge badgeContent={governanceRecord.length} color="primary"><AccountBalanceIcon /></Badge></Hidden>}
-                                                                label={<Hidden smDown><Badge badgeContent={governanceRecord.length} color="primary"><Typography variant="h6">{t('Governance')}</Typography></Badge></Hidden>
-                                                            } value="4" />
-                                                        }
+                                                        <Tab sx={{color:'white', textTransform:'none'}} 
+                                                            icon={<Hidden smUp><AccountBalanceIcon /></Hidden>}
+                                                            label={<Hidden smDown><Typography variant="h6">{t('Governance')}</Typography></Hidden>
+                                                        } value="4" />
+                                                        
                                                         {solanaDomain && 
                                                             <Tab sx={{color:'white', textTransform:'none'}} 
                                                                 icon={<Hidden smUp><Badge badgeContent={solanaDomain.length} color="primary"><LanguageIcon /></Badge></Hidden>}
@@ -1525,58 +1428,11 @@ export function IdentityView(props: any){
                                                         }
                                                     </TabPanel>
 
-                                                    <TabPanel value="4">
-                                                        {governanceRecord && 
-                                                            <div style={{ height: 600, width: '100%' }}>
-                                                                <div style={{ display: 'flex', height: '100%' }}>
-                                                                    <div style={{ flexGrow: 1 }}>
-                                                                        {publicKey && publicKey.toBase58() === pubkey ?
-                                                                            <DataGrid
-                                                                                rows={governanceRecordRows}
-                                                                                columns={governancecolumns}
-                                                                                pageSize={25}
-                                                                                rowsPerPageOptions={[]}
-                                                                                onSelectionModelChange={(newGovernanceSelectionModel) => {
-                                                                                    setSelectionGovernanceModel(newGovernanceSelectionModel);
-                                                                                }}
-                                                                                initialState={{
-                                                                                    sorting: {
-                                                                                        sortModel: [{ field: 'value', sort: 'desc' }],
-                                                                                    },
-                                                                                }}
-                                                                                sx={{
-                                                                                    borderRadius:'17px',
-                                                                                    borderColor:'rgba(255,255,255,0.25)',
-                                                                                    '& .MuiDataGrid-cell':{
-                                                                                        borderColor:'rgba(255,255,255,0.25)'
-                                                                                    }}}
-                                                                                sortingOrder={['asc', 'desc', null]}
-                                                                                disableSelectionOnClick
-                                                                            />
-                                                                        :
-                                                                        <DataGrid
-                                                                            rows={governanceRecordRows}
-                                                                            columns={governancecolumns}
-                                                                            initialState={{
-                                                                                sorting: {
-                                                                                    sortModel: [{ field: 'value', sort: 'desc' }],
-                                                                                },
-                                                                            }}
-                                                                            sx={{
-                                                                                borderRadius:'17px',
-                                                                                borderColor:'rgba(255,255,255,0.25)',
-                                                                                '& .MuiDataGrid-cell':{
-                                                                                    borderColor:'rgba(255,255,255,0.25)'
-                                                                                }}}
-                                                                            pageSize={25}
-                                                                            rowsPerPageOptions={[]}
-                                                                        />
-                                                                        }
-                                                                    </div>
-                                                                </div>
-                                                            </div>    
-                                                        }
-                                                    </TabPanel>
+                                                    {tokenMap &&
+                                                        <TabPanel value="4">
+                                                            <GovernanceView pubkey={pubkey} setLoadingPosition={setLoadingPosition} tokenMap={tokenMap} />
+                                                        </TabPanel>
+                                                    }
                                                     
                                                     <TabPanel value="5">
                                                         {/*
---
+++ src/Identity/plugins/Governance.tsx
@@ -0,0 +1,168 @@
+import React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+// @ts-ignore
+import { PublicKey, Connection } from '@solana/web3.js';
+
+import { 
+    getRealms, 
+    getTokenOwnerRecordsByOwner,
+    getTokenOwnerRecord
+} from '@solana/spl-governance';
+
+
+import GovernanceDetailsView from './GovernanceDetails';
+import { TokenAmount } from '../../utils/grapeTools/safe-math';
+import { useWallet } from '@solana/wallet-adapter-react';
+
+import {
+    Button,
+} from '@mui/material';
+
+import { GRAPE_RPC_ENDPOINT, 
+    THEINDEX_RPC_ENDPOINT, 
+    TX_RPC_ENDPOINT
+} from '../../utils/grapeTools/constants';
+
+
+const governancecolumns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70, hide: true },
+    { field: 'pubkey', headerName: 'PublicKey', width: 70, hide: true },
+    { field: 'realm', headerName: 'Realm', minWidth: 130, flex: 1, align: 'left' },
+    { field: 'governingTokenMint', headerName: 'Governing Mint', width: 150, align: 'center'},
+    { field: 'governingTokenDepositAmount', headerName: 'Votes', width: 130, align: 'center'},
+    { field: 'unrelinquishedVotesCount', headerName: 'Unreliquinshed', width: 130, align: 'center'},
+    { field: 'totalVotesCount', headerName: 'Total Votes', width: 130, align: 'center' },
+    { field: 'details', headerName: '', width: 150,  align: 'center',
+        renderCell: (params) => {
+            return (
+                <><GovernanceDetailsView governanceToken={params.value}/></>
+            )
+        }
+    },
+    { field: 'link', headerName: '', width: 150,  align: 'center',
+        renderCell: (params) => {
+            return (
+                <Button
+                    variant='outlined'
+                    size='small'
+                    component='a'
+                    href={`https://realms.today/dao/${params.value}`}
+                    target='_blank'
+                    sx={{borderRadius:'17px'}}
+                >Visit</Button>
+            )
+        }
+    },
+  ];
+
+export function GovernanceView(props: any){
+    const setLoadingPosition = props.setLoadingPosition;
+    const pubkey = props.pubkey;
+    const tokenMap = props.tokenMap;
+    const ggoconnection = new Connection(GRAPE_RPC_ENDPOINT);
+    const ticonnection = new Connection(THEINDEX_RPC_ENDPOINT);
+    const [realms, setRealms] = React.useState(null);
+    const [governanceRecord, setGovernanceRecord] = React.useState(null);
+    const [governanceRecordRows, setGovernanceRecordRows] = React.useState(null);
+    const [loadingGovernance, setLoadingGovernance] = React.useState(false);
+    const { publicKey } = useWallet();
+    const [selectionGovernanceModel, setSelectionGovernanceModel] = React.useState(null);
+    
+    const fetchGovernance = async () => {
+        setLoadingPosition('Governance');
+        const GOVERNANCE_PROGRAM_ID = 'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw';
+        const programId = new PublicKey(GOVERNANCE_PROGRAM_ID);
+        console.log("fetching getTokenOwnerRecord");
+
+        const tor = await getTokenOwnerRecord(ggoconnection, new PublicKey(pubkey));
+        console.log("tor "+JSON.stringify(tor));
+        
+        const rlms = await getRealms(ggoconnection, programId);
+        //console.log("rlms "+JSON.stringify(rlms));
+        const uTable = rlms.reduce((acc, it) => (acc[it.pubkey.toBase58()] = it, acc), {})
+        setRealms(uTable);
+        
+        const ownerRecordsbyOwner = await getTokenOwnerRecordsByOwner(ggoconnection, programId, new PublicKey(pubkey));
+    
+        console.log("ownerRecordsbyOwner "+JSON.stringify(ownerRecordsbyOwner))
+        const governance: any[] = [];
+        
+        let cnt = 0;
+        //console.log("all uTable "+JSON.stringify(uTable))
+    
+        for (const item of ownerRecordsbyOwner){
+            const realm = uTable[item.account.realm.toBase58()];
+            //console.log("realm: "+JSON.stringify(realm))
+            const name = realm.account.name;
+            let votes = item.account.governingTokenDepositAmount.toString();
+            
+            const thisToken = tokenMap.get(item.account.governingTokenMint.toBase58());
+            
+            if (thisToken){
+                votes = (new TokenAmount(+item.account.governingTokenDepositAmount, thisToken.decimals).format())
+            } else{
+                votes = 'NFT/Council';
+            }
+    
+            governance.push({
+                id:cnt,
+                pubkey:item.pubkey,
+                realm:name,
+                governingTokenMint:item.account.governingTokenMint,
+                governingTokenDepositAmount:votes,
+                unrelinquishedVotesCount:item.account.unrelinquishedVotesCount,
+                totalVotesCount:item.account.totalVotesCount,
+                details:item.account.realm.toBase58(),
+                link:item.account.realm
+            });
+            cnt++;
+        }
+    
+        setGovernanceRecord(ownerRecordsbyOwner);
+        setGovernanceRecordRows(governance);
+    }
+
+    const fetchGovernancePositions = async () => {
+        setLoadingGovernance(true);
+        await fetchGovernance();
+        setLoadingGovernance(false);
+    }
+
+    React.useEffect(() => {
+        if (pubkey && tokenMap){
+            fetchGovernancePositions();
+        }
+    }, [tokenMap]);
+
+    return(
+        <>
+        {governanceRecord && 
+            <div style={{ height: 600, width: '100%' }}>
+                <div style={{ display: 'flex', height: '100%' }}>
+                    <div style={{ flexGrow: 1 }}>
+                        
+                        <DataGrid
+                            rows={governanceRecordRows}
+                            columns={governancecolumns}
+                            initialState={{
+                                sorting: {
+                                    sortModel: [{ field: 'value', sort: 'desc' }],
+                                },
+                            }}
+                            sx={{
+                                borderRadius:'17px',
+                                borderColor:'rgba(255,255,255,0.25)',
+                                '& .MuiDataGrid-cell':{
+                                    borderColor:'rgba(255,255,255,0.25)'
+                                }}}
+                            pageSize={25}
+                            rowsPerPageOptions={[]}
+                        />
+                        
+                    </div>
+                </div>
+            </div>    
+        }
+        </>
+    );
+}
