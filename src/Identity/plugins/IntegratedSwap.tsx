7747ea67c0bd7a67daa8f7684c189d3795d627f4
Kirk <kfournia@gmail.com>
2023-01-16 00:04:05 +0200

    dependency bump

--- src/Identity/plugins/IntegratedSwap.tsx
+++ src/Identity/plugins/IntegratedSwap.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, Suspense, useCallback } from "react";
 import { AnchorWallet, useAnchorWallet, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { getPlatformFeeAccounts } from '@jup-ag/core';
+import { PublicKey } from '@solana/web3.js';
 
 import {
     Grid,
@@ -7,7 +9,7 @@ import {
 } from '@mui/material';
 
 import { GRAPE_RPC_ENDPOINT } from '../../utils/grapeTools/constants';
-  
+
 export function IntegratedSwapView(props: any){
     const refreshCallback = props.refreshCallback;
     const setLoadingPosition = props.setLoadingPosition;
@@ -15,23 +17,33 @@ export function IntegratedSwapView(props: any){
     const [loadingSwap, setLoadingSwap] = React.useState(false);
     const { publicKey, wallet } = useWallet();
     
+    /*
+    const platformFeeAndAccounts = {
+        feeBps: 10,
+        feeAccounts: await getPlatformFeeAccounts(
+        connection,
+        new PublicKey('9Y1EEuwtxk2JTe8TkvxMfA27A9sY4WVbymroSd5upGiW') // The platform fee account owner
+        ) // map of mint to token account pubkey
+    }*/
+
     const initJupiter = () => {
         if (wallet) {
             window?.Jupiter.init({
-            mode: 'default',        
-            displayMode: 'integrated',
-            integratedTargetId: 'integrated-terminal',
-            endpoint:GRAPE_RPC_ENDPOINT,
-            passThroughWallet: wallet,
-            containerStyles: {minHeight: 500},
-            onSuccess: ({ txid }) => {
-                try{
-                    console.log('onSuccess', txid);
-                    if (refreshCallback)
-                        refreshCallback();
-                }catch(err:any){console.log("ERR: "+err)}
-            }
-          });
+                mode: 'default',        
+                displayMode: 'integrated',
+                integratedTargetId: 'integrated-terminal',
+                endpoint:GRAPE_RPC_ENDPOINT,
+                passThroughWallet: wallet,
+                //platformFeeAndAccounts,
+                containerStyles: {minHeight: 500},
+                onSuccess: ({ txid }) => {
+                    try{
+                        console.log('onSuccess', txid);
+                        if (refreshCallback)
+                            refreshCallback();
+                    }catch(err:any){console.log("ERR: "+err)}
+                }
+            });
         }
     }; 
 
--- src/Identity/plugins/ModalSwap.tsx
+++ src/Identity/plugins/ModalSwap.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, Suspense, useCallback } from "react";
 import { AnchorWallet, useAnchorWallet, useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { getPlatformFeeAccounts } from '@jup-ag/core';
+import { PublicKey } from '@solana/web3.js';
 
 import {
     Button,
@@ -12,29 +14,38 @@ import { GRAPE_RPC_ENDPOINT } from '../../utils/grapeTools/constants';
 export default function ModalSwapView(props: any){
     const refreshCallback = props.refreshCallback;
     const setLoadingPosition = props?.setLoadingPosition;
+    const { connection } = useConnection();
     const [loadingSwap, setLoadingSwap] = React.useState(false);
     const { publicKey, wallet } = useWallet();
     const swapfrom = props?.swapfrom;
     const swapto = props?.swapto;
     const toTokenLabel = props?.toTokenLabel
-    
+    /*
+    const platformFeeAndAccounts = {
+        feeBps: 10,
+        feeAccounts: await getPlatformFeeAccounts(
+        connection,
+        new PublicKey('9Y1EEuwtxk2JTe8TkvxMfA27A9sY4WVbymroSd5upGiW') // The platform fee account owner
+        ) // map of mint to token account pubkey
+    }*/
+
     const initJupiter = () => {
         if (wallet) {
             window?.Jupiter.init({
-            mode: 'default',        
-            displayMode: 'modal',
-            mint: swapto,
-            endpoint:GRAPE_RPC_ENDPOINT,
-            passThroughWallet: wallet,
-            containerStyles: {height: 500},
-            onSuccess: ({ txid }) => {
-                try{
-                    console.log('onSuccess', txid);
-                    if (refreshCallback)
-                        refreshCallback();
-                }catch(err:any){console.log("ERR: "+err)}
-            }
-          });
+                mode: 'default',        
+                displayMode: 'modal',
+                mint: swapto,
+                endpoint:GRAPE_RPC_ENDPOINT,
+                passThroughWallet: wallet,
+                containerStyles: {height: 500},
+                onSuccess: ({ txid }) => {
+                    try{
+                        console.log('onSuccess', txid);
+                        if (refreshCallback)
+                            refreshCallback();
+                    }catch(err:any){console.log("ERR: "+err)}
+                }
+            });
         }
     }; 
 
