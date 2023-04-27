96015c5cc9ed54a1806a99ab2b6af82131c66c32
Kirk <kfournia@gmail.com>
2023-04-27 19:32:30 +0300

    Fixed historical price formatting

--- src/Preview/HistoryView.tsx
+++ src/Preview/HistoryView.tsx
@@ -258,7 +258,7 @@ export default function HistoryView(props: any){
                 
                 const json = await fetchMEHistoryWithTimeout(mint,0);
                 //console.log("json: "+JSON.stringify(json));
-                
+
                 try{
                     // here get the last sale and show it:
                     let found = false;
@@ -651,9 +651,11 @@ export default function HistoryView(props: any){
                                                                 
                                                             </TableCell>
                                                             <TableCell  align="right">
+                                                                    {item?.price &&
                                                                     <Typography variant="body2">
-                                                                        {(item?.price)} <SolCurrencyIcon sx={{fontSize:"10.5px"}} />
+                                                                        {((+item.price).toFixed(3))} <SolCurrencyIcon sx={{fontSize:"10.5px"}} />
                                                                     </Typography>
+                                                                    }
                                                                 </TableCell>
                                                             <TableCell align="right">
                                                                 <Typography variant="caption">
