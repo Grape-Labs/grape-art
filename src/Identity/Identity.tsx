39eecc896e0b8ff6d44d59a9853a049ec7a6032d
Kirk <kfournia@gmail.com>
2023-12-20 01:51:29 +0200

    Updated Bonfida SNS URLs

--- src/Identity/Identity.tsx
+++ src/Identity/Identity.tsx
@@ -456,7 +456,7 @@ export function IdentityView(props: any){
                                     variant='outlined'
                                     size='small'
                                     component='a'
-                                    href={params.value.indexOf(".sol") > -1 ? `https://naming.bonfida.org/domain/${params.value.slice(0,params.value.indexOf(".sol"))}`: `https://naming.bonfida.org/twitter`}
+                                    href={params.value.indexOf(".sol") > -1 ? `https://www.sns.id/domain?domain=${params.value.slice(0,params.value.indexOf(".sol"))}`: `https://naming.bonfida.org/twitter`}
                                     target='_blank'
                                     sx={{borderRadius:'17px',ml:1}}
                                 >
@@ -639,6 +639,7 @@ export function IdentityView(props: any){
 
                         }
                     }
+                    // add any assets that are not in the wallet i.e. compressed NFTs?
                 }
 
                 if (nftMeta){
